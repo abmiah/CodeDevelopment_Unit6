24f5de896d043f4c91f5df1aeda477ba4d9e2021
amrolmiah <amrolmiah@gmail.com>
2024-11-16 17:32:02 +0000

    "initial code set the admin password"

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-def message():
-    name = input("Enter your name: ")
-    if name == "Alice":
-        print("Hello, Alice, you are the best!")
-    elif name:
-        print(f"Hello, {name}!")
-    else:
-        print("Hello, World!")
-        print("You didn't enter your name. But that's okay. I'll still say hello to you.")
+#This code will autehnitcate weak password, it will use a number of libraries
 
-message()
+#The test will check the admin password, this is on the assumption that the admin of the IoT device has been set up with a password
+
+"""This class  section is to set the admin password to be checked"""
+class AdminPassword:
+    """This section sets the admin password to be checked"""
+    def __init__(self):
+        self.password = input("Please set an admin password: ")
+
+    """The class will then print the password to the console"""
+    def print_password(self):
+        print("The admin password is:", self.password)
+
+
+if __name__ == "__main__":
+    """This will run the AdminPassword class"""
+    admin = AdminPassword()
+    admin.print_password()
+
